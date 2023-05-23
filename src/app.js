234ad70e9d1098f34c45d38533a18e3c7bd53f13
Lucas Nunes <120993347+thezunes@users.noreply.github.com>
2023-05-23 00:32:44 -0300

    Initial

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+// import router from "./routes/index.routes.js";
+import cors from "cors"
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use(router);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server is running`));
